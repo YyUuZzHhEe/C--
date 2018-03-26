bf359656365a29d01f509d84e9d48bef34caeaf2
YyUuZzHhEe <1308146007@qq.com>
2018-03-26 23:03:36 +0800

    first

---
+++ 1-5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void)
+{
+	int a;
+	printf ("请输入一个整数：",a);
+	scanf ("%d",&a);
+	printf ("该整数加上12的结果是%d。",a+12);
+	return 0;
+} 
---
+++ 1-6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void)
+{
+	int a;
+	printf ("请输入一个整数：",a);
+	scanf ("%d",&a);
+	printf ("该整数减去6的结果是%d。",a-6  );
+	return 0;
+} 
